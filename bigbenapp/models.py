5157657be8a1d4f8faa7c2adeacf1d545ba6bab2
snghbeer <nivonsay.jean@hotmail.com>
2020-08-01 15:42:41 +0200

    last55

--- bigbenapp/models.py
+++ bigbenapp/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=upload_location, null=False, blank=False)
-    slug = models.SlugField(null=True)
+    slug = models.SlugField(null=True, blank=True)
 
 
     def __str__(self):
